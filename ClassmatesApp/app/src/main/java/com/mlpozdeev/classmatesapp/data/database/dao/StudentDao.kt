167b408e5a4dbd05bd57f9e5841cbb22f3507379
mlpozdeev <84346652+mlpozdeev@users.noreply.github.com>
2021-06-13 12:54:14 +0400

    Рефакторинг работы с rxjava

--- ClassmatesApp/app/src/main/java/com/mlpozdeev/classmatesapp/data/database/dao/StudentDao.kt
+++ ClassmatesApp/app/src/main/java/com/mlpozdeev/classmatesapp/data/database/dao/StudentDao.kt
@@ -4,19 +4,21 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.mlpozdeev.classmatesapp.data.database.entities.StudentEntity
+import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Observable
 
 @Dao
 interface StudentDao {
     @get:Query("SELECT * FROM student")
-    val students: Observable<List<StudentEntity>>
+    val students: Flowable<List<StudentEntity>>
 
     @Insert
-    fun insertStudent(student: StudentEntity)
+    fun insertStudent(student: StudentEntity): Completable
 
     @Insert
-    fun insertAll(students: List<StudentEntity>)
+    fun insertAll(students: List<StudentEntity>): Completable
 
     @Query("DELETE FROM student")
-    fun deleteAll()
+    fun deleteAll(): Completable
 }
